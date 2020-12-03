03fcaa1dd27c560865d8518f380fbc80370e8a6a
Bernadette Estacio <bernadette@englemaninvest.com>
2020-12-02 21:56:51 -0500

    Update url links

--- js/app.js
+++ js/app.js
@@ -1,48 +1,48 @@
 
-(function(){
+(function () {
     var app = angular.module('portfolio', []);
 
-    app.controller('IconsController', function($scope){
+    app.controller('IconsController', function ($scope) {
         $scope.mediaIcons = [
-            {href: 'https://github.com/bernadette-estacio', photo: 'img/github.png', alt: 'Github'},
-            {href: 'https://www.linkedin.com/in/bernadetteestacio/', photo: 'img/linkedinblack.png', alt: 'LinkedIn'},
-            {href: 'https://bernadetteengleman.wordpress.com/', photo: 'img/wordpress.jpg', alt: 'wordpress'}
+            { href: 'https://github.com/bernadette-estacio', photo: 'img/github.png', alt: 'Github' },
+            { href: 'https://www.linkedin.com/in/bernadetteestacio/', photo: 'img/linkedinblack.png', alt: 'LinkedIn' },
+            { href: 'https://bernadettecodes.wordpress.com/', photo: 'img/wordpress.jpg', alt: 'wordpress' }
         ];
     });
 
-    app.controller('SkillsController', ['$scope', '$http', function($scope, $http){
+    app.controller('SkillsController', ['$scope', '$http', function ($scope, $http) {
 
-        $http.get('data/codingSkills.json').success(function(data){
+        $http.get('data/codingSkills.json').success(function (data) {
             $scope.codingSkills = data;
         });
 
     }]);
 
-    app.controller('ToolsController', ['$scope', '$http', function($scope, $http){
+    app.controller('ToolsController', ['$scope', '$http', function ($scope, $http) {
 
-        $http.get('data/tools.json').success(function(data){
+        $http.get('data/tools.json').success(function (data) {
             $scope.tools = data;
         });
 
     }]);
 
-    app.controller('ProjectController', ['$scope', '$http', function($scope, $http){
+    app.controller('ProjectController', ['$scope', '$http', function ($scope, $http) {
 
-        $http.get('data/projects.json').success(function(data){
+        $http.get('data/projects.json').success(function (data) {
             $scope.projects = data;
         });
 
     }]);
 
 
-    app.directive('mediaIcons', function(){
+    app.directive('mediaIcons', function () {
         return {
             template:
-            '<span ng-repeat="icon in mediaIcons">' +
-            '<a href="{{icon.href}}" target="_blank" title="{{icon.alt}}" >' +
-            '<img src="{{icon.photo}}" alt="{{icon.alt}}" width="30" class="socialIcons">' +
-            '</a>' +
-            '</span>'
+                '<span ng-repeat="icon in mediaIcons">' +
+                '<a href="{{icon.href}}" target="_blank" title="{{icon.alt}}" >' +
+                '<img src="{{icon.photo}}" alt="{{icon.alt}}" width="30" class="socialIcons">' +
+                '</a>' +
+                '</span>'
         };
     });
 
